d2698ef1ddca17a8b44eedfe2639b7f40b624c8f
chasewilliams-tech <chase.williams@hgs.hiddengeniusproject.org>
2026-07-20 14:29:54 -0700

    Add files via upload

--- smart.py
+++ smart.py
@@ -1,6 +1,28 @@
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
+from flask import Flask, render_template, request
+import openai
 
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+app = Flask (__name__)
+
+openai.api_key = ''
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods = ["POST"])
+def api():
+
+    message = request.json.get("message" )
+
+    completion = openai.ChatCompletion.create(
+        model = "gpt-4.1-mini",
+        messages=[
+        {"role": "user", "content": message}
+        ]
+    )
+    if completion.choices[0].message!=None:
+        return completion.choices[0].message 
+    else :
+        return 'Failed to Generate response!'
+    if _name_ == '__main__':
+        app.run( )
